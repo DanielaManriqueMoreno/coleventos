ad75285c2f0901622f84ce94945aa0bf4561d4cc
Lina Vanessa Salcedo <lvb171@hotmail.com>
2025-10-22 17:03:52 -0500

    Revert "Auto stash before merge of "dev" and "origin/dev""
    This reverts commit 68008cc651e82122b679e9d00f778c21303b949c.

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\Admin\EventoController;
 use App\Http\Controllers\ArtistaController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LocalidadController;
 use App\Http\Controllers\CompraController;
 use App\Http\Controllers\EventoPublicController;
@@ -66,7 +65,6 @@ Route::prefix('artista')->group(function(){
     Route::get('/destroy/{id}', [ArtistaController::class, 'destroy'])->name('artista.destroy');
 });
 
-<<<<<<< Updated upstream
 // =============================
 // GESTIÓN DE LOCALIDADES
 // =============================
@@ -90,44 +88,3 @@ Route::prefix('evento')->group(function(){
     Route::put('/update/{id}', [EventoController::class, 'update'])->name('evento.update');
     Route::get('/destroy/{id}', [EventoController::class, 'destroy'])->name('evento.destroy');
 });
-=======
-    Route::get('/index', [DashboardController::class, 'index'])->name('admin.index');
-
-    Route::prefix('artista')->group(function(){
-        Route::get('/index', [ArtistaController::class, 'index'])->name('artista.index');
-        Route::get('/create', [ArtistaController::class, 'create'])->name('artista.create');
-        Route::get('/edit/{id}', [ArtistaController::class, 'edit'])->name('artista.edit');
-        Route::post('/store', [ArtistaController::class, 'store'])->name('artista.store');
-        Route::put('/update/{id}', [ArtistaController::class, 'update'])->name('artista.update');
-        Route::get('/destroy/{id}', [ArtistaController::class, 'destroy'])->name('artista.destroy');
-    });
-
-    Route::prefix('localidad')->group(function(){
-        Route::get('/index', [LocalidadController::class, 'index'])->name('localidad.index');
-        Route::get('/create', [LocalidadController::class, 'create'])->name('localidad.create');
-        Route::get('/edit/{id}', [LocalidadController::class, 'edit'])->name('localidad.edit');
-        Route::post('/store', [LocalidadController::class, 'store'])->name('localidad.store');
-        Route::put('/update/{id}', [LocalidadController::class, 'update'])->name('localidad.update');
-        Route::get('/destroy/{id}', [LocalidadController::class, 'destroy'])->name('localidad.destroy');
-    });
-
-    Route::prefix('evento')->group(function(){
-        Route::get('/index', [EventoController::class, 'index'])->name('evento.index');
-        Route::get('/create', [EventoController::class, 'create'])->name('evento.create');
-        Route::get('/edit/{id}', [EventoController::class, 'edit'])->name('evento.edit');
-        Route::post('/store', [EventoController::class, 'store'])->name('evento.store');
-        Route::put('/update/{id}', [EventoController::class, 'update'])->name('evento.update');
-        Route::get('/destroy/{id}', [EventoController::class, 'destroy'])->name('evento.destroy');
-    });
-
-    Route::prefix('boleteria')->group(function(){
-        Route::get('/index', [BoleteriaController::class, 'index'])->name('boleteria.index');
-        Route::get('/create', [BoleteriaController::class, 'create'])->name('boleteria.create');
-        Route::get('/edit/{id}', [BoleteriaController::class, 'edit'])->name('boleteria.edit');
-        Route::post('/store', [BoleteriaController::class, 'store'])->name('boleteria.store');
-        Route::put('/update/{id}', [BoleteriaController::class, 'update'])->name('boleteria.update');
-        Route::get('/destroy/{id}', [BoleteriaController::class, 'destroy'])->name('boleteria.destroy');
-    });
-
-});
->>>>>>> Stashed changes
