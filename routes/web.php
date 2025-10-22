b927422969efb276cdd090222960b0d3559e2655
Lina Vanessa Salcedo <lvb171@hotmail.com>
2025-10-22 17:19:21 -0500

    web
    Se corrigieron errores

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\Admin\EventoController;
+use App\Http\Controllers\EventoController;
 use App\Http\Controllers\ArtistaController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BoleteriaController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LocalidadController;
 use App\Http\Controllers\CompraController;
@@ -52,47 +53,17 @@ Route::middleware(['auth'])->group(function () {
     Route::post('/comprar', [CompraController::class, 'store'])->name('compras.store');
     // Historial de compras
     Route::get('/compras', [CompraController::class, 'index'])->name('compras.index');
-});
+});    
 
 // =============================
 //GESTIÓN DE ARTISTAS
 // =============================
-Route::prefix('artista')->group(function(){
-    Route::get('/index', [ArtistaController::class, 'index'])->name('artista.index');
-    Route::get('/create', [ArtistaController::class, 'create'])->name('artista.create');
-    Route::get('/edit/{id}', [ArtistaController::class, 'edit'])->name('artista.edit');
-    Route::post('/store', [ArtistaController::class, 'store'])->name('artista.store');
-    Route::put('/update/{id}', [ArtistaController::class, 'update'])->name('artista.update');
-    Route::get('/destroy/{id}', [ArtistaController::class, 'destroy'])->name('artista.destroy');
-});
 
-<<<<<<< Updated upstream
-// =============================
-// GESTIÓN DE LOCALIDADES
-// =============================
-Route::prefix('localidad')->group(function(){
-    Route::get('/index', [LocalidadController::class, 'index'])->name('localidad.index');
-    Route::get('/create', [LocalidadController::class, 'create'])->name('localidad.create');
-    Route::get('/edit/{id}', [LocalidadController::class, 'edit'])->name('localidad.edit');
-    Route::post('/store', [LocalidadController::class, 'store'])->name('localidad.store');
-    Route::put('/update/{id}', [LocalidadController::class, 'update'])->name('localidad.update');
-    Route::get('/destroy/{id}', [LocalidadController::class, 'destroy'])->name('localidad.destroy');
-});
-
-// =============================
-//GESTIÓN DE EVENTOS (ADMIN)
-// =============================
-Route::prefix('evento')->group(function(){
-    Route::get('/index', [EventoController::class, 'index'])->name('evento.index');
-    Route::get('/create', [EventoController::class, 'create'])->name('evento.create');
-    Route::get('/edit/{id}', [EventoController::class, 'edit'])->name('evento.edit');
-    Route::post('/store', [EventoController::class, 'store'])->name('evento.store');
-    Route::put('/update/{id}', [EventoController::class, 'update'])->name('evento.update');
-    Route::get('/destroy/{id}', [EventoController::class, 'destroy'])->name('evento.destroy');
-});
-=======
+Route::middleware(['auth'])->prefix('admin')->group(function () {
+    
+    
     Route::get('/index', [DashboardController::class, 'index'])->name('admin.index');
-
+    
     Route::prefix('artista')->group(function(){
         Route::get('/index', [ArtistaController::class, 'index'])->name('artista.index');
         Route::get('/create', [ArtistaController::class, 'create'])->name('artista.create');
@@ -100,8 +71,12 @@ Route::prefix('evento')->group(function(){
         Route::post('/store', [ArtistaController::class, 'store'])->name('artista.store');
         Route::put('/update/{id}', [ArtistaController::class, 'update'])->name('artista.update');
         Route::get('/destroy/{id}', [ArtistaController::class, 'destroy'])->name('artista.destroy');
-    });
+    });    
 
+
+// =============================
+// GESTIÓN DE LOCALIDADES
+// =============================
     Route::prefix('localidad')->group(function(){
         Route::get('/index', [LocalidadController::class, 'index'])->name('localidad.index');
         Route::get('/create', [LocalidadController::class, 'create'])->name('localidad.create');
@@ -109,25 +84,15 @@ Route::prefix('evento')->group(function(){
         Route::post('/store', [LocalidadController::class, 'store'])->name('localidad.store');
         Route::put('/update/{id}', [LocalidadController::class, 'update'])->name('localidad.update');
         Route::get('/destroy/{id}', [LocalidadController::class, 'destroy'])->name('localidad.destroy');
-    });
+    });    
 
-    Route::prefix('evento')->group(function(){
-        Route::get('/index', [EventoController::class, 'index'])->name('evento.index');
-        Route::get('/create', [EventoController::class, 'create'])->name('evento.create');
-        Route::get('/edit/{id}', [EventoController::class, 'edit'])->name('evento.edit');
-        Route::post('/store', [EventoController::class, 'store'])->name('evento.store');
-        Route::put('/update/{id}', [EventoController::class, 'update'])->name('evento.update');
-        Route::get('/destroy/{id}', [EventoController::class, 'destroy'])->name('evento.destroy');
-    });
-
-    Route::prefix('boleteria')->group(function(){
-        Route::get('/index', [BoleteriaController::class, 'index'])->name('boleteria.index');
-        Route::get('/create', [BoleteriaController::class, 'create'])->name('boleteria.create');
-        Route::get('/edit/{id}', [BoleteriaController::class, 'edit'])->name('boleteria.edit');
-        Route::post('/store', [BoleteriaController::class, 'store'])->name('boleteria.store');
-        Route::put('/update/{id}', [BoleteriaController::class, 'update'])->name('boleteria.update');
-        Route::get('/destroy/{id}', [BoleteriaController::class, 'destroy'])->name('boleteria.destroy');
-    });
+        Route::prefix('boleteria')->group(function(){
+            Route::get('/index', [BoleteriaController::class, 'index'])->name('boleteria.index');
+            Route::get('/create', [BoleteriaController::class, 'create'])->name('boleteria.create');
+            Route::get('/edit/{id}', [BoleteriaController::class, 'edit'])->name('boleteria.edit');
+            Route::post('/store', [BoleteriaController::class, 'store'])->name('boleteria.store');
+            Route::put('/update/{id}', [BoleteriaController::class, 'update'])->name('boleteria.update');
+            Route::get('/destroy/{id}', [BoleteriaController::class, 'destroy'])->name('boleteria.destroy');
+        });
 
 });
->>>>>>> Stashed changes
